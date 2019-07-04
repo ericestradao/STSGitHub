aaa7485fe9edba680c1ba8fb11fd59e66c8fb242
Eric Estrada <Eric Estrada@DESKTOP-J62LONE>
2019-07-04 12:18:45 -0600

    First commit.

---
+++ STSGitHub/src/main/java/com/example/demo/StsGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGitHubApplication.class, args);
+	}
+
+}
